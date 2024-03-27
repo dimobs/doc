ff67ba8029132e1a24ff175e4131edc4c9c893cc
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-03-27 15:30:06 +0200

    stamp added

---
+++ script.js
@@ -0,0 +1,22 @@
+let lastDate = document.getElementById('lastUpdated');
+
+// lastDate.textContent = "test1"
+
+function lastUpdated() {
+    var currentDate = new Date();
+
+    // Получаване на деня, месеца и годината от текущата дата
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1; // Месеците в JavaScript започват от 0, затова добавяме 1
+    var year = currentDate.getFullYear();
+    
+    // Форматиране на деня и месеца, за да добавим водеща нула, ако са по-малки от 10
+    day = (day < 10) ? '0' + day : day;
+    month = (month < 10) ? '0' + month : month;
+    
+    // Създаване на низ, представляващ българския формат за дата
+    var formattedDate = day + '.' + month + '.' + year;
+    lastDate.innerHTML = `${formattedDate}`
+}
+
+    lastUpdated();
